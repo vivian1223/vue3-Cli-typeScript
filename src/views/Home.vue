608cf0ec0b0b8e508c4e9e92b9a2c454fac29c30
Vivian Yeh <u4h93u6uhu@gmail.com>
2021-12-24 10:32:16 +0800

    feat: add html pug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,17 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<template lang="pug">
+table
+  thead
+    tr
+      td.table__thead 用戶編號
+      td.table__thead 用戶名稱
+      td.table__thead 會員等級
+      td.table__thead 消費額度
+  tbody
+    tr
+      td.table__tbody 0000001
+      td.table__tbody 黃小明
+      td.table__tbody 黃金
+      td.table__tbody 12304
 </template>
 
 <script lang="ts">
@@ -10,16 +19,32 @@ import {
   // defineComponent 是 TypeScript 拿來封裝 setup hook 的一個方法
   // 透過這個方法可以讓 component 被正確地型別推斷
   defineComponent,
+  ref,
 } from 'vue';
 
 export default defineComponent({
   setup() {
-    type text = string;
-    const count:text = '1';
-    console.log(count);
+    // type text = string;
+    const count = ref({ value: 1 });
     return {
       count,
     };
   },
 });
 </script>
+<style lang="sass" scoped>
+.table__text
+  font-size: 1rem
+  padding: 1.5rem 2rem
+  border: 1px #e0e0e0 solid
+
+.table__thead
+  @extend .table__text
+  background-color: #f8f9fb
+  font-weight: bold
+  color: #626262
+
+.table__tbody
+  @extend .table__text
+
+</style>
